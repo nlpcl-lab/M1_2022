f40284f36753088bd083cef9ca5282ea9f44e304
Soyeong Jeong <starsuzi@naver.com>
2022-08-12 03:07:36 +0900

    add

--- script/run_keywords_num_20-total_cluster_ver3-test_30.sh
+++ script/run_keywords_num_20-total_cluster_ver3-test_30.sh
@@ -1,7 +1,7 @@
 python /home/syjeong/Starlab/preprocess_query_qrel.py \
-    --keywords_num 5
+    --keywords_num 20
 
 python /home/syjeong/Starlab/run_bm25.py \
-    --query_path data/preprocessed/ver1/keywords_num/5/total_cluster_ver1_30_users_query_penguin.json \
-    --qrel_path data/preprocessed/ver1/keywords_num/5/total_cluster_ver1_30_users_qrel_penguin.json \
-    --index_name keywords_num_5-total_cluster_ver1-test_30
+    --query_path data/preprocessed/ver3/keywords_num/20/total_cluster_ver3_30_users_query_penguin.json \
+    --qrel_path data/preprocessed/ver3/keywords_num/20/total_cluster_ver3_30_users_qrel_penguin.json \
+    --index_name keywords_num_20-total_cluster_ver3-test_30
--- script/run_keywords_num_3-total_cluster_ver3-test_30.sh
+++ script/run_keywords_num_3-total_cluster_ver3-test_30.sh
@@ -2,6 +2,6 @@ python /home/syjeong/Starlab/preprocess_query_qrel.py \
     --keywords_num 3
 
 python /home/syjeong/Starlab/run_bm25.py \
-    --query_path data/preprocessed/ver1/keywords_num/3/total_cluster_ver1_30_users_query_penguin.json \
-    --qrel_path data/preprocessed/ver1/keywords_num/3/total_cluster_ver1_30_users_qrel_penguin.json \
-    --index_name keywords_num_3-total_cluster_ver1-test_30
+    --query_path data/preprocessed/ver3/keywords_num/3/total_cluster_ver3_30_users_query_penguin.json \
+    --qrel_path data/preprocessed/ver3/keywords_num/3/total_cluster_ver3_30_users_qrel_penguin.json \
+    --index_name keywords_num_3-total_cluster_ver3-test_30
