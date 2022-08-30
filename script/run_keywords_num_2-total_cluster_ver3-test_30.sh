27ea92bd6d2b4b4dfc116b6b56764a17bf4c84f4
Soyeong Jeong <starsuzi@naver.com>
2022-08-30 16:25:53 +0900

    add

---
+++ script/run_keywords_num_2-total_cluster_ver3-test_30.sh
@@ -0,0 +1,7 @@
+python /home/syjeong/Starlab/generate.py \
+    --keywords_num 2
+
+python /home/syjeong/Starlab/eval.py \
+    --query_path data/preprocessed/ver3/keywords_num/2/total_cluster_ver3_30_users_query_penguin.json \
+    --qrel_path data/preprocessed/ver3/keywords_num/2/total_cluster_ver3_30_users_qrel_penguin.json \
+    --index_name keywords_num_2-total_cluster_ver3-test_30
